44e8e7067de021b6507e6540145a852d91f6e919
Simon Galvis <simongalvis9@gmail.com>
2020-09-01 17:49:23 -0400

    Create Total.js

---
+++ src/Total.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './App.css'
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+class Total extends Component {
+    render(){
+
+        const total = Object.keys(this.props.selected).reduce(
+            (acc, curr) => acc + this.props.selected[curr].cost,0
+        );
+     
+
+        return(
+            <div className="summary__total">
+                <div className="summary__total__label">Total</div>
+                <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Total
